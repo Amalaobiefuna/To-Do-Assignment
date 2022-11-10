68afe8293d27932db8f1c7f681d648010ee059d9
Amalaobiefuna <obiefunaamala15@gmail.com>
2022-11-10 12:21:04 -0500

    My To-do Assignment

--- js/index.js
+++ js/index.js
@@ -11,16 +11,18 @@ function showAllToDo(toDoItem){
     list.innerHTML = `
     <input class="todo__checkbox" type="checkbox" />
     <span>${toDoItem}</span>
+    <input class="second-input" value=${toDoItem} hidden/>
     <button class="edit__btn">Edit</button>
     <button class="delete__btn">Delete</button>
     `
     const unorderedList = document.querySelector ("#todo__list")
     unorderedList.appendChild(list)
     addListenerToDelete()
+    addListenerToEdit()
 }
 
-function deleteMyToDo(clear){
-    const targetDeleteButton = clear.target
+function deleteMyToDo(e){
+    const targetDeleteButton = e.target
     const oneToDoItem = targetDeleteButton.parentNode
     oneToDoItem.remove()
     console.log (oneToDoItem)
@@ -30,6 +32,29 @@ function addListenerToDelete (){
     const deleteButtons = document.querySelectorAll (".delete__btn")
     const lastDeleteButton = deleteButtons[deleteButtons.length-1]
     lastDeleteButton.addEventListener ("click", deleteMyToDo)
+}
+
+function editMyToDo(e){
+    const targetEditButton = e.target
+    const spanElement = targetEditButton.previousElementSibling.previousElementSibling
+    const secondInput = targetEditButton.previousElementSibling
+    if(e.target.innerHTML==="Edit"){
+        targetEditButton.innerHTML="Save"
+        secondInput.hidden=false
+        spanElement.hidden=true
+        secondInput.focus()
+    }else{
+        spanElement.innerHTML=secondInput.value
+        secondInput.hidden=true
+        spanElement.hidden=false
+        targetEditButton.innerHTML="Edit"
+    }
+}
+
+function addListenerToEdit (){
+    const editButtons = document.querySelectorAll (".edit__btn")
+    const lastEditButton = editButtons[editButtons.length-1]
+    lastEditButton.addEventListener ("click", editMyToDo)
 }
 
 const toDoForm = document.querySelector (".form")
